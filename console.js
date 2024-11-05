0739042ed5ef4a45d2e7b957ed71c2bc2cdd2927
Andrew Feasel <feaseand000@student.evergreenps.org>
2024-11-05 09:21:40 -0800

    Added base64 encoding and decoding features

--- console.js
+++ console.js
@@ -1,11 +1,35 @@
 var console = {
     arr: [],
     num: 0,
-    log(source,output){
-        console.arr.push(output);
-        $(source).innerHTML += console.arr[console.num] + "<br>";
-        $(source).scrollTop = $(source).scrollHeight;
+    str: 'hello',
+    log(){
+        console.arr.push(console.str);
+        $('consoleUI').innerHTML += console.arr[console.num] + '<br>';
+        $('consoleUI').scrollTop = $('consoleUI').scrollHeight;
         console.num++;
+    },
+    del(){
+        console.arr = []; 
+        console.num = 0;
+        $('consoleUI').innerHTML = console.arr;
+        $('consoleUI').scrollTop = $('consoleUI').scrollHeight;
+    },
+    encode64(){
+        for(var i = 0; i < console.arr.length; i++){
+            console.arr[i] = btoa(console.arr[i]);
+        }
+        for(var i = 0; i < console.arr.length; i++){
+            $('consoleUI').innerHTML += console.arr[i] + '<br>';
+        }
+    },
+    decode64(){
+        for(var i = 0; i < console.arr.length; i++){
+        console.arr[i] = atob(console.arr[i]); 
+        }
+        for(var i = 0; i < console.arr.length; i++){
+            $('consoleUI').innerHTML += console.arr[i] + '<br>';
+        }
     }
+    
 };
 function $(x){return document.getElementById(x);}
