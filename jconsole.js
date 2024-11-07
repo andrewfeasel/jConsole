bcd3358631da249c3ef62bd64b6a822804e4f865
Andrew Feasel <feaseand000@student.evergreenps.org>
2024-11-06 16:02:56 -0800

    code cleanup

--- jconsole.js
+++ jconsole.js
@@ -1,53 +1,50 @@
 var jConsole = {
     arr: [],
-    num: 0,
     userInput: false,
     str: 'testString',
     //Enters a new string into the console's array, and prints it to the console
     log(){
-        if (jConsole.userInput === false){
-            jConsole.arr.push(jConsole.str.trim());
-        }else{
-            jConsole.arr.push($('input').innerHTML.trim());
+        if (this.userInput === false) {
+            this.arr.push(jConsole.str.trim());
+        } else {
+            this.arr.push($('input').value.trim());
         }
-        $('consoleUI').innerHTML += jConsole.arr[jConsole.num] + '<br>';
+        $('consoleUI').innerHTML += this.arr[this.arr.length-1] + '<br>';
         $('consoleUI').scrollTop = $('consoleUI').scrollHeight;
-        jConsole.num++;
     },
     //Deletes the console's array and the console's innerHTML
     del(){
-        jConsole.arr = []; 
-        jConsole.num = 0;
-        $('consoleUI').innerHTML = jConsole.arr;
-        $('input').innerHTML = jConsole.arr;
+        this.arr = [];
+        $('consoleUI').innerHTML = '';
+        $('input').value = '';
         $('consoleUI').scrollTop = $('consoleUI').scrollHeight;
     },
     //Normal text to Base64
     encode64(){
-        for(var i = 0; i < jConsole.arr.length; i++){
-            jConsole.arr[i] = btoa(jConsole.arr[i]);
+        for(var i = 0; i < this.arr.length; i++){
+            this.arr[i] = btoa(jConsole.arr[i]);
         }
-        for(var i = 0; i < jConsole.arr.length; i++){
-            $('consoleUI').innerHTML += jConsole.arr[i] + '<br>';
+        for(var i = 0; i < this.arr.length; i++){
+            $('consoleUI').innerHTML += this.arr[i] + '<br>';
         }
         $('consoleUI').scrollTop = $('consoleUI').scrollHeight;
     },
     //Base64 to normal text
     decode64(){
-        for(var i = 0; i < jConsole.arr.length; i++){
-            jConsole.arr[i] = atob(jConsole.arr[i]); 
+        for(var i = 0; i < this.arr.length; i++){
+            this.arr[i] = atob(jConsole.arr[i]); 
         }
-        for(var i = 0; i < jConsole.arr.length; i++){
-            $('consoleUI').innerHTML += jConsole.arr[i] + '<br>';
+        for(var i = 0; i < this.arr.length; i++){
+            $('consoleUI').innerHTML += this.arr[i] + '<br>';
         }
         $('consoleUI').scrollTop = $('consoleUI').scrollHeight;
     },
     switchinput(){
-        if(jConsole.userInput === true){
-            jConsole.userInput = false; 
+        if(this.userInput === true){
+            this.userInput = false; 
             $('settings').innerHTML = 'User input: off';
         }else{
-            jConsole.userInput = true;
+            this.userInput = true;
             $('settings').innerHTML ='User input: on';
         }
     }
