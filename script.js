1b49e01c682fa07326bf6e7bc9040695973f614a
Andrew Feasel <andrewmail404@gmail.com>
2025-05-08 09:22:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 'use strict';
+const $ = x => document.getElementById(x);
+const log = x => {
+    const ui = $("consoleUI");
+    ui.innerHTML += `${x}<br>`;
+    ui.scrollTop = ui.scrollHeight;
+};
+const clear = () => {
+    const ui = $("consoleUI");
+    ui.innerHTML = '';
+    ui.scrollTop = ui.scrollHeight;
+    $('input').value = '';
+};
 window.addEventListener('error', (error) => {
     log(`${error.name} ${error.message}`);
 });
